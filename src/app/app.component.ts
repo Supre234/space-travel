934468e24800daefa7ce1e74e3e6c5758db20dae
Supre234 <supreeth614@gmail.com>
2024-10-23 17:19:50 +0530

    added signup&footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
+import { SignupComponent } from './signup/signup.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, SignupComponent,RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  
 })
 export class AppComponent {
   title = 'space-travel';
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CardComponent } from "../components/card/card.component";
+import { FooterComponent } from "../components/footer/footer.component";
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, FooterComponent,RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
